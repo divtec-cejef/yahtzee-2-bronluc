039d29dfef0b8d3a0c8088bb1352d9694a3a8d62
Lucas Bron <lucas.bron@divtec.ch>
2025-08-20 09:23:52 +0200

    afficher les 5 dés avec leurs positions

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function lancerDe() {
     return Math.floor(Math.random() * 6) + 1;
-}
-const dés = [lancerDe(), lancerDe(), lancerDe(), lancerDe(), lancerDe()];
+    }
 
-console.log(dés);
+let des = [];
+
+for (let i = 1; i < 6; i++) {
+    let deActuel = lancerDe();
+        des.push(deActuel);
+    console.log("Dé " + i + " : " + deActuel);
+}
