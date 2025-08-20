8c8dcff2cbf854274cc9e87343ecef837a2230d2
Lucas Bron <lucas.bron@divtec.ch>
2025-08-20 11:44:38 +0200

    relancer

--- src/YahtzeeProcedural.java
+++ src/YahtzeeProcedural.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class YahtzeeProcedural {
     public static int lancerDe () {
         return (int) (Math.random() * 6) + 1;
@@ -17,7 +19,18 @@ for (int i = 0; i < liste.length; i++){
         }
     }
     public static void main(String[] args) {
+        int[] des = lancer5Des();
 
-        afficherResultatDes(lancer5Des());
+        afficherResultatDes(des);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quels dés voulez-vous relancer (0 pour terminer))");
+        int position = sc.nextInt();
+
+        while (position != 0) {
+            des[position - 1] = lancerDe();
+            position = sc.nextInt();
+        }
+        afficherResultatDes(des);
     }
 }
