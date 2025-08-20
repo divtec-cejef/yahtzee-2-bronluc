afa37b344a16d19e968c42a8ffa7a5e45efd6fc9
Lucas Bron <lucas.bron@divtec.ch>
2025-08-20 10:57:45 +0200

    final

--- src/YahtzeeProcedural.java
+++ src/YahtzeeProcedural.java
@@ -1,25 +1,17 @@
 public class YahtzeeProcedural {
     public static int lancerDe () {
-        return Math.random() * 6 + 1;
+        return (int) (Math.random() * 6) + 1;
     }
 
     public static void des () {
         int[] liste = {lancerDe(),  lancerDe(), lancerDe(), lancerDe(),  lancerDe()};
         for (int i = 0; i < liste.length; i++){
-            System.out.println("Élément " + i + " : " + liste[i]);
-
+            System.out.println("Dé " + (i + 1) + " : " + liste[i]);
         }
-
     }
 
-    public static int resultatDe () {
-    }
     public static void main(String[] args) {
 
-
-        for (int i = 0; i < 5; i++) {
-            int deActuel = lancerDe();
-            System.out.println("Dé " + i + " : " + deActuel);
-        }
+        des();
     }
 }
