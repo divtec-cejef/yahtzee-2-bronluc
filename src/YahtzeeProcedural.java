90f0d0186558ebc2761d14158de2348edc26bed5
Lucas Bron <lucas.bron@divtec.ch>
2025-08-28 11:26:26 +0200

    Jalon 4 fini mais pas corrigé

--- src/YahtzeeProcedural.java
+++ src/YahtzeeProcedural.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class YahtzeeProcedural {
 
+    static boolean afficherPaire = true;
+    static boolean afficherDoublePaires = true;
+    static boolean afficherBrelan = true;
+    static boolean afficherCarre = true;
+    static boolean afficherFullHouse = true;
+    static boolean afficherPetiteSuite = true;
+    static boolean afficherGrandeSuite = true;
+    static boolean afficherYahtzee = true;
+
     public static int lancerDe() {
         return (int) (Math.random() * 6) + 1;
     }
@@ -21,7 +30,7 @@ public class YahtzeeProcedural {
     }
 
     public static int[] demanderQuelDesLancer(Scanner sc) {
-        System.out.print("Quels dés voulez-vous relancer ?");
+        System.out.print("Quels dés voulez-vous relancer ? ");
         String saisie = sc.nextLine();
 
         if (saisie.isEmpty()) {
@@ -48,7 +57,7 @@ public class YahtzeeProcedural {
         int[] compteur = new int[6]; // indices 0 à 5 pour les faces 1 à 6
 
         for (int de : des) {
-            compteur[de - 1]++; // incrémente la case correspondant à la face
+            compteur[de - 1]++;
         }
 
         System.out.println("Occurrences des faces :");
@@ -58,11 +67,9 @@ public class YahtzeeProcedural {
         return compteur;
     }
 
-    public static void detecterCombinaisons(int[] des) {
+    public static int detecterCombinaisons(int[] des, Scanner sc, int points) {
 
         int[] compteur = compterOccurrences(des);
-
-        int points = 0;
 
         boolean paire = false;
         boolean doublePaires = false;
@@ -107,56 +114,116 @@ public class YahtzeeProcedural {
         doublePaires = nbPaires == 2;
         fullHouse = brelan && paire;
 
-        if (doublePaires) {paire = true;}
+        if (doublePaires) {
+            paire = true;
+        }
 
         System.out.println("\n");
 
-        if (paire){ points += 5;
+        if (paire && afficherPaire) {
             System.out.println("Paire : \t5 pts");
-        }else{System.out.println("Paire : \t0 pts");}
-        if (doublePaires){ points += 10;
+        } else if (afficherPaire) {
+            System.out.println("Paire : \t0 pts");
+        }
+
+        if (doublePaires && afficherDoublePaires) {
             System.out.println("Double paires : \t10 pts");
-        }else{System.out.println("Double paires : \t0 pts");}
-        if (brelan){ points += sommeBrelan;
+        } else if (afficherDoublePaires) {
+            System.out.println("Double paires : \t0 pts");
+        }
+
+        if (brelan && afficherBrelan) {
             System.out.println("Brelan : \t" + sommeBrelan + " pts");
-        }else{System.out.println("Brelan : \t0 pts");}
-        if (carre) {points += sommeCarre;
+        } else if (afficherBrelan) {
+            System.out.println("Brelan : \t0 pts");
+        }
+
+        if (carre && afficherCarre) {
             System.out.println("Carré : \t" + sommeCarre + " pts");
-        }else{System.out.println("Carré : \t0 pts");}
-        if (fullHouse) {points += 25;
+        } else if (afficherCarre) {
+            System.out.println("Carré : \t0 pts");
+        }
+
+        if (fullHouse && afficherFullHouse) {
             System.out.println("Full House : \t25 pts");
-        }else{System.out.println("Full House : \t0 pts");}
-        if (petiteSuite) {points += 30;
+        } else if (afficherFullHouse) {
+            System.out.println("Full House : \t0 pts");
+        }
+
+        if (petiteSuite && afficherPetiteSuite) {
             System.out.println("Petite suite : \t30 pts");
-        }else {System.out.println("Petite suite : \t0 pts");}
-        if (grandeSuite) {points += 40;
+        } else if (afficherPetiteSuite) {
+            System.out.println("Petite suite : \t0 pts");
+        }
+
+        if (grandeSuite && afficherGrandeSuite) {
             System.out.println("Grande suite : \t40 pts");
-        }else  {System.out.println("Grande suite : \t0 pts");}
-        if (yahtzee) {points += 50;
+        } else if (afficherGrandeSuite) {
+            System.out.println("Grande suite : \t0 pts");
+        }
+
+        if (yahtzee && afficherYahtzee) {
             System.out.println("Yahtzee : \t50 pts");
-        }else {System.out.println("Yahtzee : \t0 pts");}
+        } else if (afficherYahtzee) {
+            System.out.println("Yahtzee : \t0 pts\n");
+        }
+
+        System.out.println("Choisissez une combinaison");
+        String saisie = sc.nextLine();
+
+        if (saisie.equals("Paire")) {
+            points += 5;
+            afficherPaire = false;
+        } else if (saisie.equals("Double paires")) {
+            points += 10;
+            afficherDoublePaires = false;
+        } else if (saisie.equals("Brelan")) {
+            points += sommeBrelan;
+            afficherBrelan = false;
+        } else if (saisie.equals("Carré") || saisie.equals("Carre")) {
+            points += sommeCarre;
+            afficherCarre = false;
+        } else if (saisie.equals("Full House")) {
+            points += 25;
+            afficherFullHouse = false;
+        } else if (saisie.equals("Petite suite")) {
+            points += 30;
+            afficherPetiteSuite = false;
+        } else if (saisie.equals("Grande suite")) {
+            points += 40;
+            afficherGrandeSuite = false;
+        } else if (saisie.equals("Yahtzee")) {
+            points += 50;
+            afficherYahtzee = false;
+        }
+
+        System.out.println("Points : " + points + "\n");
+        return points;
     }
 
-    // boucle infini
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int[] des = lancer5Des();
-        int compteur = 1;
+        int points = 0;
 
-        System.out.println("Lancer " + compteur + " :");
-        afficherResultatDes(des);
+        while (true) {
+            Scanner sc = new Scanner(System.in);
 
-        while (compteur < 3) {
-            compteur++;
-            int[] positions = demanderQuelDesLancer(sc);
-            if (positions.length == 0) {
-                break;
-            }
-            relancer(des, positions);
+            int[] des = lancer5Des();
+            int compteur = 1;
+
+            System.out.println("Lancer " + compteur + " :");
             afficherResultatDes(des);
+
+            while (compteur < 3) {
+                compteur++;
+                int[] positions = demanderQuelDesLancer(sc);
+                if (positions.length == 0) {
+                    break;
+                }
+                relancer(des, positions);
+                afficherResultatDes(des);
+            }
+            points = detecterCombinaisons(des, sc, points);
         }
-        detecterCombinaisons(des);
     }
 }
